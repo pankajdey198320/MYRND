83c17076920883b9a3fde91b6dc6169aff84ec68
pankajd <pankajd@mindfiresolutions.com>
2013-05-27 18:34:54 +0530

    First commit

---
+++ DeFault Windows Service/DeFault Windows Service/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeFault_Windows_Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
