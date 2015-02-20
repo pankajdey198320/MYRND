4cd4b301ef85d8dea14bb2369c9f619c804ce3ab
pdey <pankajd@mindfiresolutions.com>
2015-02-20 15:39:50 +0530

    dd

--- CodeTests/CodetTests/CoContra_variance/Program.cs
+++ CodeTests/CodetTests/CoContra_variance/Program.cs
@@ -16,7 +16,7 @@ namespace CoContra_variance
             var sP=new PersonService<Student>();
             sP.Fill(new List<Student>() { new Student(){ ID=1, Address="banipur", Name="pankaj", RoleNo =20 },
             new Student(){ ID=2, Address="Ashoknagar", Name="Rahul", RoleNo =21 }});
-            IPersonService<Person> service = sP; ;
+            IPersonService<Person> service = sP; 
             var result = service.GetFirst();
             Console.WriteLine(result.Name);
         }
