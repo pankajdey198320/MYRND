66a6500755d041528d754ff222d5f318b08ec503
pdey <pankajd@mindfiresolutions.com>
2015-02-20 11:47:19 +0530

    Test On Action and delegate

---
+++ CodeTests/CodetTests/DelegateTest/MyCustom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTest
+{
+    class MyCustom
+    {
+        public int GetLength(string value)
+        {
+            return value.Length;
+        }
+
+        internal void DoingTheAction(string value,Action afterWork) {
+            Console.WriteLine("have done ::" + value);
+            System.Threading.Thread.Sleep(50000);
+            if (afterWork != null) {
+                afterWork();
+            }
+        }
+    }
+}
---
+++ CodeTests/CodetTests/DelegateTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCustom v = new MyCustom();
+            Func<string, int> myDele = v.GetLength;
+            Console.WriteLine(myDele("pankaj"));
+            Action<string,Action> myAction = v.DoingTheAction;
+            myAction.BeginInvoke("Hey buddy", () => {
+                Console.WriteLine("Have done enough here buddy");
+            },(x)=>{
+                if(x.IsCompleted){
+                    Console.WriteLine("bingo");
+                }
+            },null);
+            Console.ReadKey();
+        }
+    }
+}
