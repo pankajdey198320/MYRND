42c7a0ea71ef05de32abdcc36e41b6facbf65730
pdey <pankajd@mindfiresolutions.com>
2015-02-20 12:33:02 +0530

    Comareby

--- CodeTests/CodetTests/DelegateTest/MyCustom.cs
+++ CodeTests/CodetTests/DelegateTest/MyCustom.cs
@@ -8,6 +8,11 @@ namespace DelegateTest
 {
     class MyCustom
     {
+        public MyCustom(int id)
+        {
+            this.id = id;
+        }
+        public int id;
         public int GetLength(string value)
         {
             return value.Length;
@@ -19,6 +24,26 @@ namespace DelegateTest
             if (afterWork != null) {
                 afterWork();
             }
+        }
+    }
+
+
+    class MyCustomRepository:List<MyCustom>
+    {
+        public MyCustomRepository()
+        {
+            this.Add(new MyCustom(10));
+            this.Add(new MyCustom(12));
+            this.Add(new MyCustom(11));
+            this.Add(new MyCustom(1));
+            this.Add(new MyCustom(132));
+            this.Add(new MyCustom(14));
+            this.Add(new MyCustom(101));
+            this.Add(new MyCustom(19));
+            this.Add(new MyCustom(200));
+        }
+        internal int CompareBy(MyCustom t1, MyCustom t2) {
+            return t1.id.CompareTo(t2.id);
         }
     }
 }
--- CodeTests/CodetTests/DelegateTest/Program.cs
+++ CodeTests/CodetTests/DelegateTest/Program.cs
@@ -10,17 +10,30 @@ namespace DelegateTest
     {
         static void Main(string[] args)
         {
-            MyCustom v = new MyCustom();
+            MyCustom v = new MyCustom(10);
             Func<string, int> myDele = v.GetLength;
             Console.WriteLine(myDele("pankaj"));
-            Action<string,Action> myAction = v.DoingTheAction;
-            myAction.BeginInvoke("Hey buddy", () => {
+            Action<string, Action> myAction = v.DoingTheAction;
+            myAction.BeginInvoke("Hey buddy", () =>
+            {
                 Console.WriteLine("Have done enough here buddy");
-            },(x)=>{
-                if(x.IsCompleted){
+            }, (x) =>
+            {
+                if (x.IsCompleted)
+                {
                     Console.WriteLine("bingo");
                 }
-            },null);
+            }, null);
+
+            Predicate<string> po = (val) =>
+            {
+                if (val == "pankaj") return true;
+                return false;
+            };
+
+            MyCustomRepository rp = new MyCustomRepository();
+            rp.Sort(rp.CompareBy);
+
             Console.ReadKey();
         }
     }
